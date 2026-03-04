329da2cf9aa1d3c602a185634dc1dd4252a5b99a
Ratnakar Singh Parihar <ratnakarsinghparihar@Ratnakars-MacBook-Air.local>
2026-03-04 22:51:03 +0530

    add a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Routes from "./Routes";
 import { ThemeProvider } from "./components/ThemeProvider";
+import Loader from "./components/Loader"; // apna custom loader
 
 function App() {
-  return (
-    <ThemeProvider>
-      <Routes />
-    </ThemeProvider>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <ThemeProvider>{loading ? <Loader /> : <Routes />}</ThemeProvider>;
 }
 
 export default App;
